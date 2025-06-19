03c09b3582eb297f6c6c065941d7e2c6f55affc3
Miriele Silva <mirielea7777@gmail.com>
2025-06-19 09:14:08 -0400

    Initial commit

---
+++ lib/weather_package.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const _apiKey = '628ffd2574436dfaa4d39afa3feeb05b'; // coloque a chave real
+
+  static Future<double?> getTemperature(double lat, double lon) async {
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$_apiKey',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return (data['main']?['temp'] as num?)?.toDouble();
+    } else {
+      print('Erro ao buscar temperatura: ${response.statusCode}');
+      return null;
+    }
+  }
+}
+
